f3b81939b3fc04559d97f21a9b214333ee1343a2
dollarkiller <adapawang@gmail.com>
2020-03-06 10:06:22 +0800

    cli is almost done

---
+++ cli/main_generator.go
@@ -0,0 +1,7 @@
+/**
+*@program: vodka
+*@description: https://github.com/dollarkillerx
+*@author: dollarkiller [dollarkiller@dollarkiller.com]
+*@create: 2020-03-06 09:23
+ */
+package main
---
+++ cli/main_template.go
@@ -0,0 +1,7 @@
+/**
+*@program: vodka
+*@description: https://github.com/dollarkillerx
+*@author: dollarkiller [dollarkiller@dollarkiller.com]
+*@create: 2020-03-06 09:30
+ */
+package test
